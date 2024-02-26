245418c57354c62f2601069597a6c194f7bdce65
raul219 <158268622+raul219@users.noreply.github.com>
2024-02-26 11:46:41 +0100

    Update exepcions.py

--- exepcions.py
+++ exepcions.py
@@ -5,3 +5,25 @@ try:
     print(resultado)
 except:
     print("Error no se puede dividir por cero.")
+
+try:
+    numero = int(input("Introdueix un número: "))
+    print("El número introduït és:", numero)
+except ValueError:
+    print("Error si us plau, introdueix nomes numeros.")
+
+try:
+    valor1 = float(input("Introdueix el primer valor: "))
+    valor2 = float(input("Introdueix el segon valor: "))
+    resultat = valor1 + valor2
+    print(resultat)
+except ValueError:
+    print("Error")
+
+llista = [1,3,5,6]
+try:
+    num = int(input("Fica el numero de la llista"))
+    print(llista[num])
+except:
+    print("ERROR")
+    
