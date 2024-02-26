9fb13736c41e89451b102858cb6bad0909665cee
raul219 <158268622+raul219@users.noreply.github.com>
2024-02-26 11:30:18 +0100

    Update and rename exepcions.md to exepcions.py

---
+++ exepcions.py
@@ -0,0 +1,7 @@
+try:
+    num1 = int(input("Posa un numero: "))
+    num2 = int(input("Posa un numero: "))
+    resultado = num1 / num2
+    print(resultado)
+except:
+    print("Error no se puede dividir por cero.")
